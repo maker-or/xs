4516665acf0df03de0b6f841dc060e4b11ee46ac
Kalyan5252 <kalyanpendem007@gmail.com>
2025-01-24 14:18:04 +0530

    files retrieve\ api

---
+++ src/app/api/repo/[year]/[subject]/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import {eq, ilike,and } from "drizzle-orm";
+import { db } from '~/server/db';
+import { repo } from "~/server/db/schema";
+
+
+export const years = ['1','2','3','4'];
+
+export async function GET(req:NextRequest, { params }: { params: Promise<{ year: string,subject:string }> }) {
+    try {
+        const {year,subject} = (await params);
+        if(!years.includes(year)) throw new Error('Year not defined');
+
+
+        const search = await db
+        .select({ 
+            filename: repo.filename,
+            year: repo.year,  // Include the URL in the selection
+            subject: repo.subject
+        })
+        .from(repo)
+        .where(and(eq(repo.year,year),eq(repo.subject,subject)));
+        console.log(search)
+        // const url = new URL(req.url);
+        // console.log(url.searchParams);
+        return NextResponse.json({message:'success',files: JSON.stringify(search)})
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({error})
+    }
+}
+// export async function GET(){
+//     return NextResponse.json({messg: 'sucess'})
+// }
